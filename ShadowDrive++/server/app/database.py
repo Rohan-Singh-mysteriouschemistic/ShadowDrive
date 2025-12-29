30299f9a05b8488a3e35bb020b7b5aad5d4b74c2
Shabd Yadav <shabd24516@iiitd.ac.in>
2025-12-29 20:32:45 +0530

    Completion of week 2 of Backend Logic

--- ShadowDrive++/server/app/database.py
+++ ShadowDrive++/server/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 #username, password, hostname, DB
-SQLALCHEMY_DATABASE_URL = "postgresql://user:SDrive516477#@localhost/shadowdrive"
+SQLALCHEMY_DATABASE_URL = "postgresql://user:SDrive516477%23@localhost/shadowdrive"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
